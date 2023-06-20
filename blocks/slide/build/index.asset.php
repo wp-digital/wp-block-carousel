10b96ff0e4e8f54553996ea43d0327f67c34cbf9
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2023-06-20 23:18:35 +0300

    build

--- blocks/slide/build/index.asset.php
+++ blocks/slide/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '3081f09b5ed7f6f23b6d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '85fdb8b26629c73124cb');
