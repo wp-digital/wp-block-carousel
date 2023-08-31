4b611dc757be886a778627ad833a5e4659b72cc7
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2023-08-31 23:48:19 +0300

    fix pagination padding

--- blocks/carousel/build/index.asset.php
+++ blocks/carousel/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '0046f6cc9d245b75b97c');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '81f4d349659692c43c9f');
